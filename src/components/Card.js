395148d2118114ad38bbde60836e0676f5518f18
Leahnicoles <lnsimonn@hotmail.com>
2023-04-02 22:25:13 +0100

    update styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,17 @@ import Data from "./Data";
 const Card  = (props) => {
 
     return ( 
-    <div className="card">
+    <div className="card-container">
         <img className="card--img" src={props.imageUrl} alt={props.title} />
-        <FontAwesomeIcon icon={faLocationDot} className="card--location" />
-        <h2 className="card--location">{props.location}</h2>
-        <a className="card--maps-url" href={props.googleMapsUrl}>View on Google Maps</a>
-        <h3 className="card--title">{props.title}</h3>
-        <p className="card--date">{props.startDate}-{props.endDate}</p>
-        <p className="card--text">{props.description}</p>
+
+        <div className="card--details">
+            <FontAwesomeIcon icon={faLocationDot} className="card--location-symbol" />
+            <h2 className="card--location-text">{props.location}</h2>
+            <a className="card--maps-url" href={props.googleMapsUrl}>View on Google Maps</a>
+            <h3 className="card--title">{props.title}</h3>
+            <p className="card--date">{props.startDate}-{props.endDate}</p>
+            <p className="card--text">{props.description}</p>
+        </div>
     </div>
   );
 }
