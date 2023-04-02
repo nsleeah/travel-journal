2be53ea341d415ac049f8f93fcb7a795f6932476
Leahnicoles <lnsimonn@hotmail.com>
2023-04-02 17:18:30 +0100

    add data component with data

---
+++ src/components/Data.js
@@ -0,0 +1,37 @@
+const Data = [
+    
+        {
+            title: "Stamford Bridge Football Stadium",
+            location: "London, United Kingdom",
+            googleMapsUrl: "https://goo.gl/maps/PtN1Tf6as3qWtnTj6",
+            startDate: "1 Jan, 2022",
+            endDate: "31 Dec, 2022",
+            description: "Stamford Bridge Football Stadium is the home ground of Chelsea Football Club. It is located in Fulham, London and has a seating capacity of 40,834. The stadium has hosted many important matches, including FA Cup Finals, UEFA Champions League Finals and international matches.",
+            imageUrl: "https://source.unsplash.com/y8Ngwq34_Ak"
+        },
+
+        {
+            title: "American Express Community Stadium",
+            location: "Brighton, United Kingdom",
+            googleMapsUrl: "https://goo.gl/maps/8U6tnKUfvYo",
+            startDate: "1 Jan, 2022",
+            endDate: "31 Dec, 2022",
+            description: "The American Express Community Stadium, also known as the Brighton Stadium, is the home ground of Brighton & Hove Albion Football Club. It is located in Falmer, Brighton and Hove, with a seating capacity of 30,750. The stadium was opened in 2011 and has since hosted many important matches, including Premier League and international matches.",
+            imageUrl: "https://source.unsplash.com/KcOxJ5X5pC0"
+        },
+
+        {
+            title: "Camp Nou Stadium",
+            location: "Barcelona, Spain",
+            googleMapsUrl: "https://goo.gl/maps/wC8JFbCyGvL2",
+            startDate: "1 Jan, 2022",
+            endDate: "31 Dec, 2022",
+            description: "Camp Nou is the home stadium of FC Barcelona, one of the most popular football clubs in the world. The stadium has a seating capacity of 99,354, making it the largest football stadium in Europe. It has hosted many important matches, including UEFA Champions League Finals and World Cup matches.",
+            imageUrl: "https://source.unsplash.com/qslUJue6m5I"
+        },
+];
+
+
+export default Data;
+
+//Current Dates Placeholders.
